a826a5ed5d04c826f71428f302ae099df8c80adc
Tiross <Tiross@users.noreply.github.com>
2018-05-05 00:43:32 +0200

    First implementation

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 #! /usr/bin/env node
 
 'use stict';
+
+const config = require('./lib/config');
+const mqtt = require('./lib/mqtt');
+const output = require('./lib/output');
+const loop = require('./lib/loop');
+
+config.parse(require('./config.json'));
+
+if (undefined === config.bridges || !config.bridges.length) {
+  if (config.verbose) {
+    output.error('No bridges configured. Please configure at least one bridge and try again.');
+  }
+
+  process.exit(1);
+}
+
+mqtt.connect(config.broker);
+
+config.bridges.forEach((bridge) => {
+  if (!bridge || typeof(bridge.host) === 'undefined' || !bridge.host) {
+    output.error('Missing required argument "host"');
+    process.exit(1);
+  }
+
+  output.log('Polling bridge "%s" every %dms', bridge.host, bridge.interval);
+
+  loop(bridge);
+});
